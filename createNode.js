7f8341f3aca17330a29ada4ca9bb1e42d55b4e03
anon <sarah.anne.friend@gmail.com>
2019-05-06 11:03:52 -0400

    Publish on user input

--- createNode.js
+++ createNode.js
@@ -12,7 +12,6 @@ const createNode = (addrs) => {
 
   return new Promise((res) => PeerInfo.create((err, peerInfo) => res(peerInfo)))
   .then(peerInfo => {
-    //console.log(peerInfo)
     addrs.forEach((addr) => peerInfo.multiaddrs.add(addr))
     node = new MyBundle({ peerInfo })
     return new Promise((res) => node.start((err) => { if (err) { throw(err) } res(node) }))
--- run.js
+++ run.js
@@ -22,10 +22,8 @@ module.exports = () => {
   return Promise.all([
       createNode('/ip4/0.0.0.0/tcp/0'),
       createNode('/ip4/0.0.0.0/tcp/0'),
-      //createNode('/ip4/127.0.0.1/tcp/20000/ws')
     ])
   .then(nodes => {
-    //console.log(nodes)
 
     const node1 = nodes[0]
     const node2 = nodes[1]
@@ -37,19 +35,6 @@ module.exports = () => {
 
     node1.handle('/print', print)
     node2.handle('/print', print)
-    //node3.handle('/print', print)
-
-    // node1.dialProtocol(node2.peerInfo, '/print', (err, conn) => {
-    //   if (err) { throw err }
-
-    //   pull(pull.values(['node 1 dialed to node 2 successfully']), conn)
-    // })
-
-    // node2.dialProtocol(node1.peerInfo, '/print', (err, conn) => {
-    //   if (err) { throw err }
-
-    //   pull(pull.values(['node 2 dialed to node 1 successfully']), conn)
-    // })
 
     node1.once('peer:connect', (peer) => {
       console.log('connected to %s', peer.id.toB58String())
@@ -58,54 +43,34 @@ module.exports = () => {
       node1.pubsub.subscribe('news',
         (msg) => console.log(msg.from, msg.data.toString()),
         () => {
-          setInterval(() => {
-            // Publish the message on topic 'news'
-            node2.pubsub.publish(
-              'news',
-              Buffer.from('Bird bird bird, bird is the word!'),
-              () => {}
-            )
-          }, 1000)
+        //   setInterval(() => {
+        //     // Publish the message on topic 'news'
+        //     node2.pubsub.publish(
+        //       'news',
+        //       Buffer.from('Bird bird bird, bird is the word!'),
+        //       () => {}
+        //     )
+        //   }, 1000)
         }
       )
+
+    const stdin = process.openStdin();
+
+    stdin.addListener("data", function(d) {
+      // note:  d is an object, and when converted to a string it will
+      // end with a linefeed.  so we (rather crudely) account for that  
+      // with toString() and then trim() 
+      console.log("you entered: [" + 
+          d.toString().trim() + "]");
+
+      node2.pubsub.publish(
+        'news',
+        Buffer.from('Bird bird bird, bird is the word!'),
+        () => {}
+      )
+    });
+
+
+    })
   })
-  })
-
-  // parallel([
-  //   (cb) => createNode('/ip4/0.0.0.0/tcp/0', cb),
-  //   (cb) => createNode(['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/10000/ws'], cb),
-  //   (cb) => createNode('/ip4/127.0.0.1/tcp/20000/ws', cb)
-  // ], (err, nodes) => {
-  //   if (err) { throw err }
-
-  //   const node1 = nodes[0]
-  //   const node2 = nodes[1]
-  //   const node3 = nodes[2]
-
-  //   printAddrs(node1, '1')
-  //   printAddrs(node2, '2')
-  //   printAddrs(node3, '3')
-
-  //   node1.handle('/print', print)
-  //   node2.handle('/print', print)
-  //   node3.handle('/print', print)
-
-  //   node1.dialProtocol(node2.peerInfo, '/print', (err, conn) => {
-  //     if (err) { throw err }
-
-  //     pull(pull.values(['node 1 dialed to node 2 successfully']), conn)
-  //   })
-
-  //   node2.dialProtocol(node3.peerInfo, '/print', (err, conn) => {
-  //     if (err) { throw err }
-
-  //     pull(pull.values(['node 2 dialed to node 3 successfully']), conn)
-  //   })
-
-  //   node3.dialProtocol(node1.peerInfo, '/print', (err, conn) => {
-  //     if (err) {
-  //       console.log('node 3 failed to dial to node 1 with:', err.message)
-  //     }
-  //   })
-  // })
 }
